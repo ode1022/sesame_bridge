a63772f4cb4e48b53f69c046083a68ead99c4462
hashimoto <hashimoto@kanamei.co.jp>
2021-02-17 01:55:49 +0900

    fix: API POST時のレスポンスでリダイレクトするのをやめてstatus:200でbodyをOKと返すように変更。Webページからのsubmit時のみリダイレクトに。

--- app/src/main/java/jp/ode/sesamebridge/MyService.kt
+++ app/src/main/java/jp/ode/sesamebridge/MyService.kt
@@ -57,7 +57,7 @@ window.addEventListener('load', function() {
 """
             }
             var body = """<html><head><meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script></head><body>"""
-            body += """${toggled}<form action="./" method="post"><div class="mx-auto" style="width: max-content;margin-top: 200px"><input type="submit" name="submit" value="Sesameトグル開閉" class="btn btn-primary"></div></form>"""
+            body += """${toggled}<form action="./" method="post"><input type="hidden" name="web" value="1"><div class="mx-auto" style="width: max-content;margin-top: 200px"><input type="submit" name="submit" value="Sesameトグル開閉" class="btn btn-primary"></div></form>"""
             // body += """<div class="container mx-auto"><form action="./" method="post" style="position: absolute; top: 50%; left: 50%; transform: transform: translateY(-50%) translateX(-50%);"><input type="submit" name="submit" value="Sesameトグル開閉">${toggled}</form></div>"""
             body += "</html>"
             response.send(body)
@@ -66,7 +66,11 @@ window.addEventListener('load', function() {
         server.post("/", HttpServerRequestCallback {request, response ->
             Log.d("test", "request start")
             sesameToggle()
-            response.redirect("./?toggled=y")
+            if (!request["web"].isNullOrBlank()) {
+                response.redirect("./?toggled=y")
+            } else {
+                response.send("ok")
+            }
             Log.d("test", "request end")
         })
         // listen on port 5000
